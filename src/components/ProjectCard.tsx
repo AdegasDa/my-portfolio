1c3d679141fe77f7038337bc75df9b2d96944ad8
AdegasDa <danieladegass@hotmail.com>
2024-06-21 13:03:19 +0100

    links corrected & icons uploaded

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -28,56 +28,50 @@ const ProjectCard: React.FC<ProjectCardInterface> = ({ title, detail, technologi
     }
 
     return (
-        <motion.div
-            initial={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.2 }}
-            whileHover={{ opacity: 1, scale: 1.2 }}
-        >
-            <div className='project-card bg-custom-dark p-6 flex flex-col h-full shadow-xl rounded-md'>
-                <div className='flex justify-between'>
-                    <div>
-                        <button onClick={() => downloadFileAtUrl(link_folder)}><FontAwesomeIcon className='h-9 text-custom-light' icon={faFolder} /></button>
-                    </div>
-                    <div className='my-auto'>
-                        <p className='text-custom-light'>
-                            {date_start}/{date_end}
-                        </p>
-                    </div>
-                    <div>
-                        <a href={link_gitHub} target='_blank'><FontAwesomeIcon className='h-9 text-custom-light' icon={faGithub} /></a>
-                    </div>
+        <div className='project-card bg-custom-dark p-6 flex flex-col h-full shadow-xl rounded-md'>
+            <div className='flex justify-between'>
+                <div>
+                    <button onClick={() => downloadFileAtUrl(link_folder)}><FontAwesomeIcon className='h-9 text-custom-light hover:text-custom-primary' icon={faFolder} /></button>
                 </div>
-                <div className='my-5'>
-                    <motion.div
-                    initial={{ opacity: 1, scale: 1 }}
-                    transition={{ duration: 0.2 }}
-                    whileHover={{ opacity: 1, scale: 2 }}
-                    >
-                    <img src={image_1} alt="" />
-                    </motion.div>
-                </div>
-                <div className='flex-grow'>
-                    <h5 className='text-custom-light font-bold mb-4'>{title}</h5>
-                    <p className='text-custom-primary'>
-                        {detail}
+                <div className='my-auto'>
+                    <p className='text-custom-light'>
+                        {date_start}/{date_end}
                     </p>
                 </div>
                 <div>
-                    <p className='text-custom-primary mt-1'>
-                        <span className='font-semibold'>Technologies Used</span>:
-                        {technologies.map((tech, index) => {
-                            if (index < technologies.length - 2) {
-                                return (' ' + tech + ',');
-                            } else if (index === technologies.length - 2) {
-                                return (' ' + tech + ' and ');
-                            } else {
-                                return (tech + ".");
-                            }
-                        })}
-                    </p>
+                    <a href={link_gitHub} target='_blank'><FontAwesomeIcon className='h-9 text-custom-light hover:text-custom-primary' icon={faGithub} /></a>
                 </div>
             </div>
-        </motion.div>
+            <div className='my-5'>
+                <motion.div
+                initial={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.2 }}
+                whileHover={{ opacity: 1, scale: 2.6 }}
+                >
+                <img src={image_1} alt="" className='boxShadow rounded-md'/>
+                </motion.div>
+            </div>
+            <div className='flex-grow'>
+                <h5 className='text-custom-light font-bold mb-4'>{title}</h5>
+                <p className='text-custom-primary'>
+                    {detail}
+                </p>
+            </div>
+            <div>
+                <p className='text-custom-primary mt-1'>
+                    <span className='font-semibold'>Technologies Used</span>:
+                    {technologies.map((tech, index) => {
+                        if (index < technologies.length - 2) {
+                            return (' ' + tech + ',');
+                        } else if (index === technologies.length - 2) {
+                            return (' ' + tech + ' and ');
+                        } else {
+                            return (tech + ".");
+                        }
+                    })}
+                </p>
+            </div>
+        </div>
     )
 }
 
--- src/pages/Home/sections/Contact/Contact.tsx
+++ src/pages/Home/sections/Contact/Contact.tsx
@@ -2,6 +2,10 @@ import '../../../../index.css'
 
 import Section from '../../../../components/Section';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
+
 const Contact = () => {
     return (
         <>
@@ -12,7 +16,17 @@ const Contact = () => {
                         <h4 className="text-3xl mx-auto text-custom-dark mb-4 font-semibold">Get In Touch</h4>
                         <p className="mx-auto">Email: danieladegass@hotmail.com</p>
                         <p className="mx-auto">Mobile: (+351) 910881658</p>
-                        <a href="https://www.linkedin.com/in/daniel-adegas-946615252/" target='_blank' className='mx-auto text-custom-dark hover:text-custom-secondary underline underline-offset-2'>LinkdIn</a>
+                        <div className='flex flex-row gap-4 justify-center'>
+                            <div className='flex justify-center items-center flex-row gap-2'>
+                                <FontAwesomeIcon className='text-custom-secondary h-5' icon={faLinkedin}/>
+                                <a href="https://www.linkedin.com/in/daniel-adegas-946615252/" target='_blank' className=' text-custom-dark hover:text-custom-secondary underline underline-offset-2'>LinkdIn</a>
+                            </div>
+                            <div className='flex justify-center items-center flex-row gap-2'>
+                                <FontAwesomeIcon className='text-custom-secondary h-5' icon={faGithubSquare}/>
+                                <a href="https://github.com/AdegasDa" target='_blank' className=' text-custom-dark hover:text-custom-secondary underline underline-offset-2'>GitHub</a>
+                            </div>
+                            
+                        </div>
                     </div>
                     <div className="mx-auto pb-3">
                         Developed & designed by Daniel Adegas.
